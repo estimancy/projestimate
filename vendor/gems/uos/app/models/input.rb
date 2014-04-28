25d042c06f93b1795e555331eaff20ca620de5a8
nicolasrenard <renard760@gmail.com>
2014-04-28 10:25:17 +0200

    UOS/technologies and synthesis Import/Export improvement.

--- vendor/gems/uos/app/models/input.rb
+++ vendor/gems/uos/app/models/input.rb
@@ -4,12 +4,11 @@ class Input < ActiveRecord::Base
   def self.export(mp, pbs)
     @inputs = Input.where(module_project_id: mp, pbs_project_element_id: pbs).all
     csv_string = CSV.generate(:col_sep => I18n.t(:general_csv_separator)) do |csv|
-      csv << ['id', 'Name', 'Technology', 'UO', 'Complexity', 'Size Low', 'Size Most Likely', 'Size High', 'Weight', 'Gross Low', 'Gross Most Likely', 'Gross High' ]
+      csv << ['id', 'Name', 'Technology', 'UO', 'Complexity', 'Size Low', 'Size Most Likely', 'Size High', 'Weight']
       @inputs.each do |i|
         csv << ["#{i.id}", "#{i.name}", "#{i.technology_id}", "#{i.unit_of_work_id}", "#{i.complexity_id}",
                 "#{i.size_low}", "#{i.size_most_likely}", "#{i.size_high}",
-                "#{i.weight}",
-                "#{i.gross_low}", "#{i.gross_most_likely}", "#{i.gross_high}"]
+                "#{i.weight}"]
       end
     end
     csv_string.encode(I18n.t(:general_csv_encoding))
@@ -31,9 +30,6 @@ class Input < ActiveRecord::Base
             @ware.update_attribute('size_most_likely', row[6])
             @ware.update_attribute('size_high', row[7])
             @ware.update_attribute('weight', row[8])
-            @ware.update_attribute('gross_low', row[9])
-            @ware.update_attribute('gross_most_likely', row[10])
-            @ware.update_attribute('gross_high', row[11])
           rescue
             error_count = error_count + 1
           end
