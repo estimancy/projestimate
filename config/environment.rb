ade07d3fec10f8d4562b0e477be3e32afaa8728b
gsaly <gueyesaly@yahoo.fr>
2014-06-25 11:01:17 +0200

    Security test, updating the travis config refs:#738

--- config/environment.rb
+++ config/environment.rb
@@ -42,7 +42,6 @@ Projestimate::Application.initialize!
 #config.autoload_paths << File.join(config.root, "lib")
 
 #Encoding.default_external = Encoding::UTF_8
-#Encoding.default_internal = Encoding::UTF_8
 
 COMMIT_VERSION = `git rev-list -n1 --abbrev-commit HEAD` unless defined? COMMIT_VERSION
 
