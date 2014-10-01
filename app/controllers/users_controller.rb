b3623c7eecf49063fdcb94c7297cc07659bc3eec
nicolasrenard <renard760@gmail.com>
2014-10-01 16:41:12 +0200

    test security

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -72,7 +72,13 @@ public
     authorize! :manage, User
 
     set_page_title 'Users'
-    @users = current_user.organizations.map{|i| i.users }.flatten.uniq
+
+    #No authorize required since everyone can list
+    if current_user.groups.map(&:name).include?("MasterAdmin")
+      @users = User.all
+    else
+      @users = current_user.organizations.map{|i| i.users }.flatten.uniq
+    end
 
     @audits = Audit.all
   end
@@ -84,6 +90,7 @@ public
 
     @user = User.new(:user_status => 'active')
     @user.auth_type = AuthMethod.first.id
+
     @users = current_user.organizations
 
   end
