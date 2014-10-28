3710586eec7c5db7f930e9a14684d7dc795f4e8c
nicolasrenard <renard760@gmail.com>
2014-10-28 16:07:36 +0100

    remove project observer

--- config/application.rb
+++ config/application.rb
@@ -92,7 +92,7 @@ module Projestimate
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
     # Activate observers that should always be running.
-    config.active_record.observers = :project_observer, :user_observer, :permission_observer, :module_project_observer
+    config.active_record.observers = :user_observer, :permission_observer, :module_project_observer
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
