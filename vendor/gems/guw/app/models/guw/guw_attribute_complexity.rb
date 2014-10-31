67d04162221825fcaed4313d0e8725a682860bc1
nicolasrenard <renard760@gmail.com>
2014-10-31 11:40:41 +0100

    Generic Unit Of Work

--- vendor/gems/guw/app/models/guw/guw_attribute_complexity.rb
+++ vendor/gems/guw/app/models/guw/guw_attribute_complexity.rb
@@ -2,10 +2,9 @@ module Guw
   class GuwAttributeComplexity < ActiveRecord::Base
     belongs_to :guw_type
 
-    validates_presence_of :name, :weight, :bottom_range, :top_range, :guw_type_id
+    validates_presence_of :name, :bottom_range, :top_range, :value, :guw_type_id
 
     validates :bottom_range, numericality: { only_integer: true }
     validates :top_range, numericality: { only_integer: true }
-    validates :weight, numericality: { only_integer: true }
   end
 end
