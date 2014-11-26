3019fe3359219d2b26d5abac84fd621b0bc58091
gsaly <gueyesaly@yahoo.fr>
2014-11-26 18:20:23 +0100

    Support Sparkom

---
+++ app/assets/javascripts/support_sparkom.js
@@ -0,0 +1,77 @@
+
+var multiWidget = new SpkMultiWidget(helperId);
+
+function onWidgets(widgets) {
+    var html = '<div class="dropdown clearfix"><button style="width:200px;" class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">';
+    html += 'Accès Conseillers';
+    html += '&nbsp;&nbsp;<span class="caret"></span></button>';
+
+    html += '<ul class="dropdown-menu" style="width:200px;" role="menu" aria-labelledby="dropdownMenu1">';
+    for(var i=0; i < widgets.length; i++) {
+        var wi = widgets[i];
+        var hline = '<li role="presentation">';
+        hline += '<a style="color:#206A7C;" role="menuitem" tabindex="-1" href="';
+        hline += widgetLaunchUrl(wi.widgetId);
+        hline += '">';
+        if(wi.available) {
+            hline += '<span class="glyphicon glyphicon-user" aria-hidden="true" style="color:green;"></span> ';
+        } else {
+            hline += '<span class="glyphicon glyphicon-user" aria-hidden="true" style="color:red;"></span> ';
+        }
+        hline += wi.displayName;
+        hline += ' </a>';
+
+        hline += '<li class="dropdown-submenu">';
+        hline += '<a role="menuitem" class="dropdown-toggle" style="text-align:right;font-size:80%;color:#6396A3;" tabindex="-1" href="';
+        hline += '#';
+        hline += '"> ';
+        hline += 'Voir les conseillers ';
+        hline += '<span class="glyphicon glyphicon-chevron-right" style="color:#6396A3;" aria-hidden="true"></span>';
+        hline += '</a>';
+        /*
+         hline += '<ul class="dropdown-menu">';
+         for(var j=0; j < wi.members.length; j++) {
+         hline += '<li><a style="color:#206A7C;" role="menuitem" tabindex="-1" href="';
+         hline += memberLaunchUrl(wi.widgetId, wi.members[j].laid);
+         hline += '">';
+         hline += wi.members[j].displayName;
+         hline += ' </a></li>';
+         }
+         hline += '</ul>';
+         */
+
+        hline += '</li>';
+        html += '\n' + hline;
+    }
+    html += '</ul></div>';
+
+    $('#spktargetdiv').html(html);
+    onSpkError("");
+}
+function onSpkError(errMessage) {
+    $('#errordiv').html(errMessage);
+}
+
+$(document).ready(function() {
+    //REAL CODE:
+    var multiWidget = new SpkMultiWidget(helperId);
+    multiWidget.onWidgets(onWidgets);
+    multiWidget.onError(onSpkError);
+    multiWidget.start();
+
+});
+
+// May use different displays
+function widgetLaunchUrl(widgetId) {
+    var tUrl = PAN_SERVER_BASE_URL + 'wchat/sparkom/wchat.html?wid=';
+    tUrl = tUrl + widgetId;
+    tUrl = 'javascript:window.open(\''+ tUrl + '\', \'chat\', \'scrollbars=no,location=no,width=700,height=450,location=no,menubar=no,status=no,toolbar=no\');';
+    return tUrl;
+}
+function memberLaunchUrl(widgetId, laid) {
+    var tUrl = PAN_SERVER_BASE_URL + 'wchat/sparkom/wchat.html?wid=';
+    tUrl = tUrl + widgetId;
+    tUrl += '&tolaid=' + laid;
+    tUrl = 'javascript:window.open(\''+ tUrl + '\', \'chat\', \'scrollbars=no,location=no,width=700,height=450,location=no,menubar=no,status=no,toolbar=no\');';
+    return tUrl;
+}
