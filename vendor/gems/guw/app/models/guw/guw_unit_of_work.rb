c4002170d9f8d69fb8ce7f020872fff874ca0ad5
nicolasrenard <renard760@gmail.com>
2015-01-06 10:51:38 +0100

    Guw Model

--- vendor/gems/guw/app/models/guw/guw_unit_of_work.rb
+++ vendor/gems/guw/app/models/guw/guw_unit_of_work.rb
@@ -4,7 +4,7 @@ module Guw
     belongs_to :guw_type
     belongs_to :guw_model
     belongs_to :guw_complexity
-    has_many :guw_unit_of_work_attributes
+    has_many :guw_unit_of_work_attributes, dependent: :destroy
 
     validates_presence_of :name, :guw_type_id, :guw_unit_of_work_group_id
 
