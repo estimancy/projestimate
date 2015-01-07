e5e6a7f93424608ebd76c5f15be84b55cee6b688
nicolasrenard <renard760@gmail.com>
2015-01-07 17:37:42 +0100

    settings

--- app/assets/javascripts/projestimate_script.js
+++ app/assets/javascripts/projestimate_script.js
@@ -2246,26 +2246,26 @@ var warnLeavingUnsavedMessage;
 function warn_me(message){
     warnLeavingUnsavedMessage = message;
     $('.simple_form').submit(function(){
-        $('textarea').not("table.tablesorterPager textarea").not("#send_feedback_description").not(".tree_node_checkbox").not(".uow_form input").removeData('changed');
-        $('input').not("table.tablesorterPager input").not("#send_feedback_user_name").not(".tree_node_checkbox").not(".uow_form input").removeData('changed');
+        $('textarea').not("table.tablesorterPager textarea").not("#send_feedback_description").not(".tree_node_checkbox").not(".uow_form input").not(".table_guw_model input").removeData('changed');
+        $('input').not("table.tablesorterPager input").not("#send_feedback_user_name").not(".tree_node_checkbox").not(".uow_form input").not(".table_guw_model input").removeData('changed');
     });
-    $('textarea').not("table.tablesorterPager textarea").not("#send_feedback_description").not(".tree_node_checkbox").not(".uow_form input").change(function(){
+    $('textarea').not("table.tablesorterPager textarea").not("#send_feedback_description").not(".tree_node_checkbox").not(".uow_form input").not(".table_guw_model input").change(function(){
         $(this).data('changed', 'changed');
     });
-    $('input').not("table.tablesorterPager input").not("#send_feedback_user_name").not(".tree_node_checkbox input").not(".uow_form input").change(function(){
+    $('input').not("table.tablesorterPager input").not("#send_feedback_user_name").not(".tree_node_checkbox input").not(".uow_form input").not(".table_guw_model input").change(function(){
         $(this).data('changed', 'changed');
     });
 
     window.onbeforeunload = function(){
         var warn = false;
-        $('textarea').not("table.tablesorterPager textarea").not("#send_feedback_description").not(".tree_node_checkbox").not(".uow_form input").blur().each(function(){
+        $('textarea').not("table.tablesorterPager textarea").not("#send_feedback_description").not(".tree_node_checkbox").not(".uow_form input").not(".table_guw_model input").blur().each(function(){
             if ($(this).data('changed')) {
                 warn = true;
             }
         });
 
 
-        $('input').not("table.tablesorterPager input").not("#send_feedback_user_name").not(".tree_node_checkbox").not(".uow_form input").blur().each(function(){
+        $('input').not("table.tablesorterPager input").not("#send_feedback_user_name").not(".tree_node_checkbox").not(".uow_form input").not(".table_guw_model input").blur().each(function(){
             if ($(this).data('changed')) {
                 warn = true;
             }
