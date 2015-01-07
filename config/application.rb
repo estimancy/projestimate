479bc8daca103ef6dc93a758d57696b5dd3136c9
gsaly <gueyesaly@yahoo.fr>
2015-01-07 16:02:52 +0100

    View list in estimation plan>edit  refs:#822

--- config/application.rb
+++ config/application.rb
@@ -174,7 +174,9 @@ module Projestimate
 
     GLOBAL_WIDGETS_TYPE = [ ["", [  ["Simple text", "text"], ["Line chart", "line_chart"], ["Bar chart", "bar_chart"], ["Area chart", "area_chart"], ["Pie chart","pie_chart"], ["Timeline", "timeline"], ["Stacked bar chart", "stacked_bar_chart"] ] ] ]
 
-    BREAKDOWN_WIDGETS_TYPE = [ ["Effort per phase", [ ["Table", "table_effort_per_phase"], ["Histogram", "histogram_effort_per_phase"], ["Pie chart", "pie_chart_effort_per_phase"] ] ],
+    BREAKDOWN_WIDGETS_TYPE = [
+                               ["", [  ["Simple text", "text"], ["Line chart", "line_chart"], ["Bar chart", "bar_chart"], ["Area chart", "area_chart"], ["Pie chart","pie_chart"], ["Timeline", "timeline"], ["Stacked bar chart", "stacked_bar_chart"] ] ],
+                               ["Effort per phase", [ ["Table", "table_effort_per_phase"], ["Histogram", "histogram_effort_per_phase"], ["Pie chart", "pie_chart_effort_per_phase"] ] ],
                                ["Cost per phase", [ ["Table", "table_cost_per_phase"], ["Histogram", "histogram_cost_per_phase"], ["Pie chart", "pie_chart_cost_per_phase"] ] ],
                                ["Effort per phases and profiles", [ ["Table", "effort_per_phases_profiles_table"],["Stacked bar chart", "stacked_bar_chart_effort_per_phases_profiles"] ] ],
                                ["Cost per phases and profiles", [ ["Table", "cost_per_phases_profiles_table"], ["Stacked bar chart", "stacked_bar_chart_cost_per_phases_profiles"] ] ]
