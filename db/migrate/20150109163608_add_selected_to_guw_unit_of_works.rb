bbabbf50bf967a546ecd01782277548c228fe449
nicolasrenard <renard760@gmail.com>
2015-01-12 11:09:46 +0100

    comment migration

--- db/migrate/20150109163608_add_selected_to_guw_unit_of_works.rb
+++ db/migrate/20150109163608_add_selected_to_guw_unit_of_works.rb
@@ -1,5 +1,5 @@
 class AddSelectedToGuwUnitOfWorks < ActiveRecord::Migration
   def change
-    add_column :guw_guw_unit_of_works, :selected, :boolean
+    #add_column :guw_guw_unit_of_works, :selected, :boolean
   end
 end
