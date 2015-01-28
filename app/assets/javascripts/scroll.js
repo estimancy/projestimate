1953f4eaf2668ad1b8faae2cf6e530ec392efce9
nicolasrenard <renard760@gmail.com>
2015-01-28 10:27:50 +0100

    Scroll et pull-left des modules

---
+++ app/assets/javascripts/scroll.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    window.scroll(0, getPageScroll());
+});
+
+//this function simply gets the window scroll position, works in all browsers
+function getPageScroll() {
+    var yScroll;
+    if (self.pageYOffset) {
+        yScroll = self.pageYOffset;
+    } else if (document.documentElement && document.documentElement.scrollTop) {
+        yScroll = document.documentElement.scrollTop;
+    } else if (document.body) {
+        yScroll = document.body.scrollTop;
+    }
+    return yScroll
+}
