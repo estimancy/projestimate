1ddb40dcdb329e5d9818c8e468f98fc045eb6933
nicolasrenard <renard760@gmail.com>
2015-01-28 16:41:48 +0100

    display value enhancement

--- app/models/module_project.rb
+++ app/models/module_project.rb
@@ -165,7 +165,12 @@ class ModuleProject < ActiveRecord::Base
   def size
     module_alias = self.pemodule.alias
     if module_alias == "ge"
-      self.ge_model.retained_size_unit
+      previous_module_project = self.previous.first
+      if previous_module_project.pemodule.alias == "expert_judgement"
+        previous_module_project.expert_judgement_instance.retained_size_unit
+      else
+        previous_module_project.guw_model.retained_size_unit
+      end
     elsif module_alias == "guw"
       self.guw_model.retained_size_unit
     elsif module_alias == "expert_judgement"
