a9894a7413b383c16a2318e2fceea98c2cb2c1fd
nicolasrenard <renard760@gmail.com>
2015-02-10 17:56:03 +0100

    ihm modification

--- db/migrate/20150210144659_add_disabled_input_to_estimation_models.rb
+++ db/migrate/20150210144659_add_disabled_input_to_estimation_models.rb
@@ -1,6 +1,6 @@
 class AddDisabledInputToEstimationModels < ActiveRecord::Migration
   def change
-    add_column :guw_guw_models, :enabled_input, :boolean
+    add_column :ge_ge_models, :enabled_input, :boolean
     add_column :expert_judgement_instances, :enabled_input, :boolean
     add_column :wbs_activities, :enabled_input, :boolean
   end
