b8d2fe04ea1466a8e0b1d2762a6c37c0482c5e69
gsaly <gueyesaly@yahoo.fr>
2015-02-25 11:55:23 +0100

    Gestion et Réorganization des droits et des redirections

--- app/controllers/views_widgets_controller.rb
+++ app/controllers/views_widgets_controller.rb
@@ -192,7 +192,7 @@ class ViewsWidgetsController < ApplicationController
   def destroy
     @views_widget = ViewsWidget.find(params[:id])
     #if can?(:alter_estimation_plan, @project) || ( can?(:manage_estimation_widgets, @project) { |widget| widget.project_fields.empty? } )
-    if can?(:alter_estimation_plan, @project) || ( can?(:manage_estimation_widgets, @project) if @views_widget.project_fields.empty? )
+    if can?(:alter_estimation_plan, @project) || ( can?(:manage_estimation_widgets, @project) && @views_widget.project_fields.empty? )
       @views_widget.destroy
     else
       flash[:warning] = I18n.t(:notice_cannot_delete_widgets)
