9647c808a18f58c76338fa5ebdc5bcc976fb9a6b
nicolasrenard <renard760@gmail.com>
2015-06-18 16:48:15 +0200

    Correction lors de la mise à jour d'un UO

--- vendor/gems/guw/app/controllers/guw/guw_unit_of_works_controller.rb
+++ vendor/gems/guw/app/controllers/guw/guw_unit_of_works_controller.rb
@@ -68,12 +68,10 @@ class Guw::GuwUnitOfWorksController < ApplicationController
 
   def update
     @guw_unit_of_work = Guw::GuwUnitOfWork.find(params[:id])
-    if params[:guw_unit_of_work][:organization_id].present?
-      if @guw_unit_of_work.update_attributes(params[:guw_unit_of_work])
-        redirect_to main_app.dashboard_path(@project, anchor: "accordion#{@guw_unit_of_work.guw_unit_of_work_group.id}") and return
-      else
-        render :edit
-      end
+    if @guw_unit_of_work.update_attributes(params[:guw_unit_of_work])
+      redirect_to main_app.dashboard_path(@project, anchor: "accordion#{@guw_unit_of_work.guw_unit_of_work_group.id}") and return
+    else
+      render :edit
     end
   end
 
