354a2ce01043ce5666303c795f1d50e5327f76a4
nicolasrenard <renard760@gmail.com>
2015-07-17 15:56:09 +0200

    Correction bug stelsia

--- config/application.rb
+++ config/application.rb
@@ -177,3 +177,5 @@ module Projestimate
                                       "pie_chart_cost_per_phase", "stacked_bar_chart_effort_per_phases_profiles",  "stacked_bar_chart_cost_per_phases_profiles" ]
   end
 end
+
+APP_CONFIG = YAML.load(IO.read(Rails.root.join("config", "sensitive_settings.yml")))
