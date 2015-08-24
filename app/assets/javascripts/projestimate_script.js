19c90a3c9f2262800fe30f896fcc435d187aa486
nicolasrenard <renard760@gmail.com>
2015-08-24 11:16:31 +0200

    Cotation

--- app/assets/javascripts/projestimate_script.js
+++ app/assets/javascripts/projestimate_script.js
@@ -19,6 +19,10 @@
 
 $(document).ready(function() {
 
+    $(".modal").draggable({
+        handle: ".modal-header"
+    });
+
     // Modal bug
     $('.modal-backdrop').remove();
     $(".modal").hide();
@@ -983,4 +987,5 @@ jQuery.fn.submitWithAjax = function () {
         return false;
     });
 };
+
 
