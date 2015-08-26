e8db2eedd49f076dcae228028b8904fe1c413726
nicolasrenard <renard760@gmail.com>
2015-08-26 14:59:26 +0200

    savesort & display order

--- db/migrate/20150826071618_add_display_order_to_tables.rb
+++ db/migrate/20150826071618_add_display_order_to_tables.rb
@@ -1,7 +1,7 @@
 class AddDisplayOrderToTables < ActiveRecord::Migration
   def change
-    add_column :guw_guw_complexities, :display_order, :integer
-    add_column :guw_guw_work_units, :display_order, :integer
-    add_column :guw_guw_type_complexities, :display_order, :integer
+    add_column :guw_guw_complexities, :display_order, :integer, :default => 0
+    add_column :guw_guw_work_units, :display_order, :integer, :default => 0
+    add_column :guw_guw_type_complexities, :display_order, :integer, :default => 0
   end
 end
