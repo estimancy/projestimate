9af353d6ffba8ea61a773ce0289de3fd1fb4510c
nicolasrenard <renard760@gmail.com>
2015-09-09 15:03:10 +0200

    SAML

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,8 +272,24 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   ###config.omniauth :google_oauth2, "504921486051.apps.googleusercontent.com", "AIzaSyDBsz9qmhKS8oalbkJwTqKsBi8yKAnfwmw", { access_type: "offline", approval_prompt: "Acceptez-vous de vous connecter à ProjEstimate via votre compte Google" }
+
+CERTIFICATE =<<CERTIFICATE
+-----BEGIN CERTIFICATE-----
+MIIDFDCCAfygAwIBAgIEVLPsKDANBgkqhkiG9w0BAQ0FADBMMQswCQYDVQQGEwJGUjENMAsGA1UE CgwEU05DRjENMAsGA1UECwwERFNJVDEfMB0GA1UEAwwWaWRwLXNpZ25hdHVyZS0yMDE0LWRldjAe Fw0xNTAxMTIxNTQ2NThaFw0yNTAxMTIxNTQ2NThaMEwxCzAJBgNVBAYTAkZSMQ0wCwYDVQQKDART TkNGMQ0wCwYDVQQLDAREU0lUMR8wHQYDVQQDDBZpZHAtc2lnbmF0dXJlLTIwMTQtZGV2MIIBIjAN BgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArV7qnTenVeZwYP4m0ji5xbsFvqNRTW8+i0Ys98oB VKlWJpEXdPUQMBi4TdjHPUBRmueW74v1v8Uw+1NeE8WvI0bStvH7P2zxeP5bdL6onVNIZUdb1L1l kBjlYQP30TtsRZuJ2d+vmf3BKEvtd3V47A8gSuAJO9q8dT+Rby7ZOMWw/ZU/dJTIGplhgpJlQMXi 3wLZyHU+oi7V5PmRE0ZYEn0LLXtQXQj1bYW+5AjU6TykXQVqISqImGiONpnKQYkOgZ56vXR9nU+/ ZSmyc/VTBTnA0Xwj/aWfOokaFqft0LhH1gykhq9IIgHaxo55SqRm4lymxx13Hpe1lA3BlWzWVQID AQABMA0GCSqGSIb3DQEBDQUAA4IBAQCisxQCBp26pa52WPq5l0srOO6kfq0oNi/6IWKlQQF0p8MV gsH+ITyHd59h5b5JXXUirL+FzpTHiabLVcSkA2rG+jfuqCGTMtXZiANUEkmlNa8NAmDHwOPe19fL SFl7duFE5BacS4IYf6zPFkpMNBMBJdQlud+chuiKpdYQanSN2Vr+ZEUQlYddkUFZUGIKpw3LpwRP dSdxoqNyHr4u+j5yOXWzaRtZpaw7zMPNT+YAB6shmpVSsbwJ1xRE34t1hj3UV/44XnnvXWCqUjsp yUje5NYfEh8ehf5Ysv6kToXM9fTyKWJhx+ip/lnSIzNe1bFjGXesSmYGtapu6htcLF2m
+-----END CERTIFICATE-----
+CERTIFICATE
+
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
   config.omniauth :google_oauth2, "504921486051-vb2gi8tfaff5hqgnnosamjto3sv5gabp.apps.googleusercontent.com", "wLnNbhyZGrdkcjnxWe95SRr7", { access_type: "offline", approval_prompt: I18n.t(:text_ask_connect_to_estimancy_via_google) }
-  config.omniauth :saml, idp_cert_fingerprint: 'fingerprint', idp_sso_target_url: 'https://idp-dev.sncf.fr/openam/SSOPOST/metaAlias/IDP/DEFAUT'
+  config.omniauth :saml,
+                  idp_cert_fingerprint: 'fingerprint',
+                  idp_sso_target_url: APP_CONFIG['IDP_SSO_TARGET_URL'],
+                  issuer: APP_CONFIG['ISSUER'],
+                  :assertion_consumer_service_url => APP_CONFIG['ASSERTION_CONSUMER_SERVICE_URL'],
+                  :name_identifier_format => APP_CONFIG['NAME_IDENTIFIER_FORMAT'],
+                  :idp_cert => CERTIFICATE,
+                  :idp_sso_target_url_runtime_params  => { :original_request_param => :mapped_idp_param }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
