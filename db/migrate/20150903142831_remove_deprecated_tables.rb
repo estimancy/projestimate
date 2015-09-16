6a6e9d081dd78cfc1d95e083db7ec6fa13e43df9
nicolasrenard <renard760@gmail.com>
2015-09-16 19:09:14 +0200

    Gemfile and correction

--- db/migrate/20150903142831_remove_deprecated_tables.rb
+++ db/migrate/20150903142831_remove_deprecated_tables.rb
@@ -1,8 +1,8 @@
 class RemoveDeprecatedTables < ActiveRecord::Migration
   def change
-    drop_table :factor_translations
-    drop_table :labor_categories
-    drop_table :labor_categories_project_areas
-    drop_table :master_settings
+    # drop_table :factor_translations
+    # drop_table :labor_categories
+    # drop_table :labor_categories_project_areas
+    # drop_table :master_settings
   end
 end
