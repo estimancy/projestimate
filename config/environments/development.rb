e0f8278f13982b6f25ffaaacdcb03e800a957b7f
nicolasrenard <renard760@gmail.com>
2015-09-21 09:45:21 +0200

    GUW

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Projestimate::Application.configure do
   config.action_controller.perform_caching = false
 
   # Print deprecation notices to the Rails logger
-  config.active_support.deprecation = :log
+  config.active_support.deprecation = :info
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
