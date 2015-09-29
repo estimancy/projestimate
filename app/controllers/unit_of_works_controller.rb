bb8d7828fc76cb7a0bd380149758ee808941cb12
golit_j <goliti_j@epitech.eu>
2015-09-29 16:19:08 +0200

    chasse au page sans nom

--- app/controllers/unit_of_works_controller.rb
+++ app/controllers/unit_of_works_controller.rb
@@ -26,6 +26,7 @@ class UnitOfWorksController < ApplicationController
   def index
     authorize! :show_unit_of_works, UnitOfWork
 
+    set_page_title 'Unite of work'
     @organization = Organization.find(params[:organization_id])
     @unit_of_works = @organization.unit_of_works
   end
