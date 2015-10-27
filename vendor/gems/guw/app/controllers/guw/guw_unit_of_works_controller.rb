367c5ce8f8f11d1780c8ba49dadb0aee312a1274
nicolasrenard <renard760@gmail.com>
2015-10-27 13:56:14 +0100

    Setting bugs

--- vendor/gems/guw/app/controllers/guw/guw_unit_of_works_controller.rb
+++ vendor/gems/guw/app/controllers/guw/guw_unit_of_works_controller.rb
@@ -342,13 +342,12 @@ class Guw::GuwUnitOfWorksController < ApplicationController
       tcplx = Guw::GuwComplexityTechnology.where(guw_complexity_id: guw_complexity_id,
                                                  organization_technology_id: guw_unit_of_work.organization_technology_id).first
 
-      if guw_unit_of_work.guw_complexity.nil?
-        @weight_pert << (cwu.nil? ? 1 : cwu.value.to_f) * (tcplx.nil? ? 0 : tcplx.coefficient.to_f) * 1
-      else
-        @weight_pert << (cwu.nil? ? 1 : cwu.value.to_f) * (tcplx.nil? ? 0 : tcplx.coefficient.to_f) * guw_unit_of_work.guw_complexity.weight.to_f
-      end
-      guw_unit_of_work.effort = @weight_pert.sum * guw_unit_of_work.quantity.to_f
-      #end
+      # if guw_unit_of_work.guw_complexity.nil?
+        # @weight_pert << (cwu.nil? ? 1 : cwu.value.to_f) * (tcplx.nil? ? 0 : tcplx.coefficient.to_f) * 1
+      # else
+        # @weight_pert << (cwu.nil? ? 1 : cwu.value.to_f) * (tcplx.nil? ? 0 : tcplx.coefficient.to_f) * guw_unit_of_work.guw_complexity.weight.to_f
+      # end
+      # guw_unit_of_work.effort = @weight_pert.sum * guw_unit_of_work.quantity.to_f
 
       if guw_unit_of_work.guw_type.allow_retained == false
         guw_unit_of_work.ajusted_effort = guw_unit_of_work.effort
