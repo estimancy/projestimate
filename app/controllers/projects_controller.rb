1d73be2da3ea8200f4c8b6033e0a9b56da7288c4
nicolasrenard <renard760@gmail.com>
2015-11-10 14:35:20 +0100

    Bugsss

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -152,7 +152,9 @@ class ProjectsController < ApplicationController
 
     elsif @module_project.pemodule.alias == "kb"
       @kb_model = current_module_project.kb_model
-      @kb_input = Kb::KbInput.where(module_project_id: current_module_project.id).last
+      @kb_input = Kb::KbInput.where(module_project_id: @module_project.id,
+                                    organization_id: @organization.id,
+                                    kb_model_id: @kb_model.id).first_or_create
       @project_list = []
     elsif @module_project.pemodule.alias == "ge"
       @ge_model = current_module_project.ge_model
