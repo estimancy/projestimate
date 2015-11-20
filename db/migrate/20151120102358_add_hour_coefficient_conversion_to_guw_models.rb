50040bc5c3b6a0b12b6b7adc7234c3711c827b01
nicolasrenard <renard760@gmail.com>
2015-11-20 11:33:26 +0100

    Correction

--- db/migrate/20151120102358_add_hour_coefficient_conversion_to_guw_models.rb
+++ db/migrate/20151120102358_add_hour_coefficient_conversion_to_guw_models.rb
@@ -1,5 +1,5 @@
 class AddHourCoefficientConversionToGuwModels < ActiveRecord::Migration
   def change
-    add_column :guw_models, :hour_coefficient_conversion, :float
+    add_column :guw_guw_models, :hour_coefficient_conversion, :float
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151118085254) do
+ActiveRecord::Schema.define(:version => 20151120102358) do
 
   create_table "abacus_organizations", :force => true do |t|
     t.float    "value"
@@ -326,6 +326,36 @@ ActiveRecord::Schema.define(:version => 20151118085254) do
 
   add_index "estimation_values", ["links"], :name => "index_attribute_projects_on_links"
 
+  create_table "event_types", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "icon_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "uuid"
+    t.integer  "record_status_id"
+    t.string   "custom_value"
+    t.integer  "owner_id"
+    t.text     "change_comment"
+    t.integer  "reference_id"
+    t.string   "reference_uuid"
+  end
+
+  add_index "event_types", ["record_status_id"], :name => "index_event_types_on_record_status_id"
+  add_index "event_types", ["reference_id"], :name => "index_event_types_on_parent_id"
+  add_index "event_types", ["uuid"], :name => "index_event_types_on_uuid", :unique => true
+
+  create_table "events", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.integer  "event_type_id"
+    t.integer  "project_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "expert_judgement_instance_estimates", :force => true do |t|
     t.integer "pbs_project_element_id"
     t.integer "module_project_id"
@@ -358,6 +388,17 @@ ActiveRecord::Schema.define(:version => 20151118085254) do
     t.boolean "enabled_size"
     t.integer "copy_id"
   end
+
+  create_table "factor_translations", :force => true do |t|
+    t.integer  "factor_id"
+    t.string   "locale",     :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.text     "helps"
+  end
+
+  add_index "factor_translations", ["factor_id"], :name => "index_factor_translations_on_factor_id"
+  add_index "factor_translations", ["locale"], :name => "index_factor_translations_on_locale"
 
   create_table "factors", :force => true do |t|
     t.string   "name"
@@ -501,14 +542,15 @@ ActiveRecord::Schema.define(:version => 20151118085254) do
     t.string   "name"
     t.text     "description"
     t.integer  "organization_id"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                 :null => false
+    t.datetime "updated_at",                                 :null => false
     t.boolean  "three_points_estimation"
     t.string   "retained_size_unit"
     t.boolean  "one_level_model"
     t.integer  "copy_id"
-    t.integer  "copy_number",             :default => 0
+    t.integer  "copy_number",                 :default => 0
     t.string   "coefficient_label"
+    t.float    "hour_coefficient_conversion"
   end
 
   create_table "guw_guw_type_complexities", :force => true do |t|
@@ -639,6 +681,24 @@ ActiveRecord::Schema.define(:version => 20151118085254) do
     t.integer "copy_id"
   end
 
+  create_table "labor_categories", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "uuid"
+    t.integer  "record_status_id"
+    t.string   "custom_value"
+    t.integer  "owner_id"
+    t.text     "change_comment"
+    t.integer  "reference_id"
+    t.string   "reference_uuid"
+  end
+
+  add_index "labor_categories", ["record_status_id"], :name => "index_labor_categories_on_record_status_id"
+  add_index "labor_categories", ["reference_id"], :name => "index_labor_categories_on_parent_id"
+  add_index "labor_categories", ["uuid"], :name => "index_labor_categories_on_uuid", :unique => true
+
   create_table "labor_categories_project_areas", :id => false, :force => true do |t|
     t.integer  "labor_category_id"
     t.integer  "project_area_id"
@@ -720,6 +780,21 @@ ActiveRecord::Schema.define(:version => 20151118085254) do
     t.string  "operation_type"
     t.integer "copy_id"
     t.integer "copy_number"
+  end
+
+  create_table "organization_labor_categories", :force => true do |t|
+    t.integer  "organization_id"
+    t.integer  "labor_category_id"
+    t.string   "level"
+    t.string   "name"
+    t.text     "description"
+    t.float    "cost_per_hour"
+    t.integer  "base_year"
+    t.integer  "currency_id"
+    t.float    "hour_per_day"
+    t.integer  "days_per_year"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "organization_profiles", :force => true do |t|
@@ -967,6 +1042,21 @@ ActiveRecord::Schema.define(:version => 20151118085254) do
     t.integer  "project_area_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "profile_categories", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "organization_id"
+    t.string   "uuid"
+    t.integer  "record_status_id"
+    t.string   "custom_value"
+    t.integer  "owner_id"
+    t.text     "change_comment"
+    t.integer  "reference_id"
+    t.string   "reference_uuid"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
   end
 
   create_table "profiles", :force => true do |t|
