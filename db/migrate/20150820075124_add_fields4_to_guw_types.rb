6377aeb0e9d7a8fad477641363a02a3f07661162
nicolasrenard <renard760@gmail.com>
2015-11-27 15:23:37 +0100

    Correction

--- db/migrate/20150820075124_add_fields4_to_guw_types.rb
+++ db/migrate/20150820075124_add_fields4_to_guw_types.rb
@@ -1,5 +1,5 @@
 class AddFields4ToGuwTypes < ActiveRecord::Migration
   def change
-    add_column :guw_guw_types, :allow_criteria, :boolean
+    add_column :guw_guw_types, :allow_criteria, :boolean, default: true
   end
 end
