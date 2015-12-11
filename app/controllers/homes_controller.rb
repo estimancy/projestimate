b8ab06e1176fdb1eeaedef8fe01ae8c9413ff22a
nicolasrenard <renard760@gmail.com>
2015-12-11 14:11:39 +0100

    Correction apres livraison

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -18,7 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ########################################################################
-
+=begin
 class HomesController < ApplicationController
 
   def update_install
--- app/models/home.rb
+++ app/models/home.rb
@@ -18,7 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 #############################################################################
-
+=begin
 require 'open-uri'
 require 'mysql2'
 
