dbecc515a352e1d3065874efd3139d91ef089073
nicolasrenard <renard760@gmail.com>
2016-02-08 10:38:15 +0100

    Amélioration du module de taille générique.

--- vendor/gems/guw/app/controllers/guw/guw_models_controller.rb
+++ vendor/gems/guw/app/controllers/guw/guw_models_controller.rb
@@ -602,7 +602,7 @@ class Guw::GuwModelsController < ApplicationController
     @guw_model.organization_id = params[:guw_model][:organization_id].to_i
 
     if @guw_model.save
-      redirect_to main_app.organization_module_estimation_path(@guw_model.organization_id, anchor: "taille")
+      redirect_to redirect_apply(guw.edit_guw_model_path(@guw_model, organization_id: @organization.id), guw.guw_model_path(@guw_model))
     else
       render action: :new
     end
@@ -616,11 +616,10 @@ class Guw::GuwModelsController < ApplicationController
 
     if @guw_model.update_attributes(params[:guw_model])
       if @guw_model.default_display == "list"
-        redirect_to guw.guw_model_all_guw_types_path(@guw_model) and return
+        redirect_to redirect_apply(guw.edit_guw_model_path(@guw_model, organization_id: @organization.id), guw.guw_model_all_guw_types_path(@guw_model)) and return
       else
-        redirect_to guw.guw_model_path(@guw_model) and return
+        redirect_to redirect_apply(guw.edit_guw_model_path(@guw_model, organization_id: @organization.id), guw.guw_model_path(@guw_model)) and return
       end
-      redirect_to main_app.organization_module_estimation_path(@guw_model.organization_id, anchor: "taille")
     else
       render action: :edit
     end
