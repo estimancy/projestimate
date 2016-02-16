576cb00d5ca952871ba65f3c965eff973c8d1a89
nicolasrenard <renard760@gmail.com>
2016-02-16 16:53:51 +0100

    Merge branch 'dev' of github.com:estimancy/projestimate into dev
    # Conflicts:
    #	db/schema.rb

--- db/migrate/20160216095512_add_allow_technology_to_guw_guw_models.rb
+++ db/migrate/20160216095512_add_allow_technology_to_guw_guw_models.rb
@@ -1,5 +1,5 @@
 class AddAllowTechnologyToGuwGuwModels < ActiveRecord::Migration
   def change
-    add_column :guw_guw_models, :allow_technology, :boolean
+    add_column :guw_guw_models, :allow_technology, :boolean, default: true
   end
 end
