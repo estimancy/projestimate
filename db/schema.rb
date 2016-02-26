3e2db6f67183187f2d06abeae79192f414e069e4
nicolasrenard <renard760@gmail.com>
2016-02-26 15:05:24 +0100

    Merge branch 'dev' of github.com:estimancy/projestimate into dev
    # Conflicts:
    #	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -486,10 +486,10 @@ ActiveRecord::Schema.define(:version => 20160225125011) do
     t.string   "p_calculation_method"
     t.string   "s_calculation_method"
     t.string   "c_calculation_method"
-    t.integer  "input_pe_attribute_id"
-    t.integer  "output_pe_attribute_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "input_pe_attribute_id"
+    t.integer  "output_pe_attribute_id"
   end
 
   create_table "groups", :force => true do |t|
@@ -660,14 +660,14 @@ ActiveRecord::Schema.define(:version => 20160225125011) do
     t.string   "name"
     t.text     "description"
     t.integer  "organization_technology_id"
-    t.datetime "created_at",                                   :null => false
-    t.datetime "updated_at",                                   :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
     t.integer  "guw_model_id"
     t.integer  "copy_id"
     t.boolean  "allow_quantity"
-    t.boolean  "allow_retained",             :default => true
+    t.boolean  "allow_retained"
     t.boolean  "allow_complexity"
-    t.boolean  "allow_criteria",             :default => true
+    t.boolean  "allow_criteria"
   end
 
   create_table "guw_guw_unit_of_work_attributes", :force => true do |t|
@@ -1587,7 +1587,7 @@ ActiveRecord::Schema.define(:version => 20160225125011) do
     t.boolean  "super_admin",            :default => false
     t.boolean  "password_changed"
     t.text     "description"
-    t.datetime "subscription_end_date",  :default => '2017-01-12 10:03:08'
+    t.datetime "subscription_end_date",  :default => '2016-11-25 14:37:58'
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
