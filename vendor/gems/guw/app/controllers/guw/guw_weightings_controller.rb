669aafbf2edf361db4041f1e13e5c36d614ef061
nicolasrenard <renard760@gmail.com>
2016-03-31 14:13:25 +0200

    Bug ratio et redirection guw_weightings

--- vendor/gems/guw/app/controllers/guw/guw_weightings_controller.rb
+++ vendor/gems/guw/app/controllers/guw/guw_weightings_controller.rb
@@ -34,7 +34,7 @@ class Guw::GuwWeightingsController < ApplicationController
 
     set_breadcrumbs I18n.t(:organizations) => "/organizationals_params",
                     @current_organization.to_s => main_app.organization_estimations_path(@current_organization),
-                    @guw_model => guw.edit_guw_model_path(@guw_work_unit.guw_model, organization_id: @guw_weighting.guw_model.organization.id)
+                    @guw_model => guw.edit_guw_model_path(@guw_weighting.guw_model, organization_id: @guw_weighting.guw_model.organization.id)
 
     set_page_title I18n.t(:Create_a_new_Work_Unit)
   end
@@ -45,7 +45,7 @@ class Guw::GuwWeightingsController < ApplicationController
 
     set_breadcrumbs I18n.t(:organizations) => "/organizationals_params",
                     @current_organization.to_s => main_app.organization_estimations_path(@current_organization),
-                    @guw_model => guw.edit_guw_model_path(@guw_work_unit.guw_model, organization_id: @guw_weighting.guw_model.organization.id)
+                    @guw_model => guw.edit_guw_model_path(@guw_weighting.guw_model, organization_id: @guw_weighting.guw_model.organization.id)
 
     set_page_title I18n.t(:Edit_a_new_Work_Unit)
   end
